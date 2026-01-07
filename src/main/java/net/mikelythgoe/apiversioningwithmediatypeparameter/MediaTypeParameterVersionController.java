a570abf75f0eee76efa0afc44b8e05af1e8799f0
mikelythgoe <github@mikelythgoe.net>
2026-01-07 08:11:22 +0000

    Added curl example text to one method, made responses objects

--- src/main/java/net/mikelythgoe/apiversioningwithmediatypeparameter/MediaTypeParameterVersionController.java
+++ src/main/java/net/mikelythgoe/apiversioningwithmediatypeparameter/MediaTypeParameterVersionController.java
@@ -10,37 +10,40 @@ public class MediaTypeParameterVersionController {
     // http://localhost:8080/hello?version=1.0 (you can use 1.0, 1, or 1.0.0, but you can't use 1.1, 1.0.1, etc.')
     @GetMapping(value = "/hello",  version = "1.0", produces = "application/json")
     public String helloV1point0() {
-        return "Hello Version One";
+        return "{\"message\":\"Hello Version 1.0\"}";
     }
 
     // http://localhost:8080/hello - Accept=application/json;version=2.0 (you can use 2.0, 2, or 2.0.0, but you can't use 2.1, 2.0.1, etc.')
     @GetMapping(value = "/hello", version = "2.0", produces = "application/json")
     public String helloV2point0Json() {
-        return "Hello Version Two JSON";
+        return "{\"message\":\"Hello Version 2.0\"}";
     }
 
     // http://localhost:8080/hello - Accept=application/xml;version=2.0 (you can use 2.0, 2, or 2.0.0, but you can't use 2.1, 2.0.1, etc.')
+    // Example: curl --location 'http://localhost:8080/hello' --header 'Accept: application/xml;version=2'
     @GetMapping(value = "/hello", version = "2.0", produces = "application/xml")
     public String helloV2point0Xml() {
-        return "Hello Version Two XML";
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" +
+                " <root>\n" +
+                "     <message>Hello Version 1</message>\n" +
+                " </root>";
     }
 
     // http://localhost:8080/hello & application/json  (you can use 3.5, or 3.5.0, but you can't use 3, 3.6., 3.5.1, etc.)
     @GetMapping(value = "/hello", version = "3.5", produces = "application/json")
     public String helloV3point5() {
-        return "Hello Version 3.5";
-    }
+        return "{\"message\":\"Hello Version 3.5\"}";    }
 
     // http://localhost:8080/hello?version=9 ( you can use 9, 9.0, or 9.0.0, but you can't use 9.1, 9.0.1, etc.)
     @GetMapping(value = "/hello", version = "9")
     public String helloV9() {
-        return "Hello Version 9";
+        return "{\"message\":\"Hello Version 9\"}";
     }
 
     // http://localhost:8080/hello?version=9 ( you can use 9.9, or 9.9.0, but you can't use 9.9.1, etc.)
     @GetMapping(value = "/hello", version = "9.9")
     public String helloV9point9() {
-        return "Hello Version 9.9";
+        return "{\"message\":\"Hello Version 9.9\"}";
     }
 }
 
