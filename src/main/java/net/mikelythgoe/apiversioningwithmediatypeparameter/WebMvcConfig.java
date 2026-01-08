f7440f8d1da4bfb2b7a3a8158bd27588a41c8d4f
mlythgoe <github@mikelythgoe.net>
2026-01-08 22:48:27 +0000

    Got WebMvcConfig working

--- src/main/java/net/mikelythgoe/apiversioningwithmediatypeparameter/WebMvcConfig.java
+++ src/main/java/net/mikelythgoe/apiversioningwithmediatypeparameter/WebMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ApiVersionConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration - Can't get the class config working, still using properties
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     public void configureApiVersioning(ApiVersionConfigurer configurer) {
@@ -14,8 +14,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addSupportedVersions("1.0", "2.0", "3.5", "9") // Add Supported Versions defines what versions can be supplied
                 .setDefaultVersion("1.0") // Used when no version, or an unsupported version, is passed in the request
 
-                .useMediaTypeParameter(MediaType.APPLICATION_XML, "Accept")
-                .useMediaTypeParameter(MediaType.APPLICATION_JSON, "Accept"); // the name of the query parameter that supplies the version
+                .useMediaTypeParameter(MediaType.APPLICATION_XML, "version") // e.g. Accept = application/xml;version=3.5
+                .useMediaTypeParameter(MediaType.APPLICATION_JSON, "version"); //e.g. Accept = application/json;version=3.5
 
 
     }
