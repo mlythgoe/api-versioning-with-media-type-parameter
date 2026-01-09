db09237642730e68a6f9ba349587ca907fdba390
mlythgoe <github@mikelythgoe.net>
2026-01-09 19:35:02 +0000

    Got WebMvcConfig working

--- src/main/java/net/mikelythgoe/apiversioningwithmediatypeparameter/WebMvcConfig.java
+++ src/main/java/net/mikelythgoe/apiversioningwithmediatypeparameter/WebMvcConfig.java
@@ -13,7 +13,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
         configurer
                 .addSupportedVersions("1.0", "2.0", "3.5", "9") // Add Supported Versions defines what versions can be supplied
                 .setDefaultVersion("1.0") // Used when no version, or an unsupported version, is passed in the request
-
                 .useMediaTypeParameter(MediaType.APPLICATION_XML, "version") // e.g. Accept = application/xml;version=3.5
                 .useMediaTypeParameter(MediaType.APPLICATION_JSON, "version"); //e.g. Accept = application/json;version=3.5
 
